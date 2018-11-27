923ecf0d9d7dd0da227e2b48a7ec97ccc0dbeb66
Charles Blouin <cha.blouin@gmail.com>
2018-11-27 01:36:20 +0000

    Added equations for friction

--- servorobots/components/test_motor.py
+++ servorobots/components/test_motor.py
@@ -7,18 +7,19 @@ import pybullet_data
 import time
 
 class GearedDcMotor:
-    def __init__(self, R, Kv, K_viscous):
+    def __init__(self, R, Kv, K_viscous, K_load):
         # Motor internal resistance
         self.R = R
         self.Kv = Kv
         self.K_viscous = K_viscous
+        self.K_load = K_load
 
     def torque_from_voltage(self, Vin, omega):
 
         # Torque output of the motor before gear
         # Nm = V/Ohm / (rad/s/V) = I * V * s = W * s = Nm/s * s
         torque_raw = (Vin - omega / self.Kv) / self.R / self.Kv
-        print(torque_raw)
+
         # Kt = Kv
         # Current = Torque * Kt = Torque * Kv
         # A = Nm * (rad/s/V) = W*s / V / s = V * A * s / V / s = A
@@ -26,10 +27,28 @@ class GearedDcMotor:
 
         torque_friction = omega * self.K_viscous
 
-        torque = torque_raw # - torque_friction
+        torque_out = (torque_raw - torque_friction) / (1 + self.K_load)
 
+        # To find K_viscous, run the motor with the gearbox attached and not other load.
+        # Note the motor speed and the current. Then, calculate Kv with :
+        # Kv = omega / (Vin - current * R) <==> current * R = Vin - omega/ Kv
+        # With no load, torque_raw = torque_viscous. Calculate torque_raw with
+        # torque_raw = current / Kv
+        # Then, calculate K_viscous with
+        # K_viscous = torque_raw/omega
+
+
+
+        # To find K_load when the motor is pulling a known load torque_out.
+        # torque_out = torque_raw - torque_friction - torque_out * K_load
+        # torque_out (1 + K_load) =  torque_raw - torque_friction
+        # torque_out = (torque_raw - torque_friction) / (1 + K_load) < True in general for this model
+        # K_load = (torque_raw - torque_friction)/torque_out - 1
+        # K_load = ((Vin - omega / self.Kv) / self.R / self.Kv) - omega * self.K_viscous)/torque_out - 1
+
+        print(torque_out)
         # Motor current
-        return torque, current
+        return torque_out, current
 
 class MotorTest:
     def __init__(self):
@@ -42,7 +61,7 @@ class MotorTest:
         self.frictionId = p.addUserDebugParameter("jointFriction", 0, 20, 0)
         self.torqueId = p.addUserDebugParameter("joint torque", 0, 20, 5)
 
-        self.motor_1 = GearedDcMotor(R=4, Kv=12, K_viscous=0.00)
+        self.motor_1 = GearedDcMotor(R=4, Kv=12, K_viscous=0.000122489, K_load=0)
 
         self.display_joint_torque = p.addUserDebugText('NA', [1.5, 0, 0.1], textColorRGB=[0, 0, 0],
                                                   textSize=1)
