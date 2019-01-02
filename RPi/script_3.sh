ae829fca105be49c2d53545fc7dc1197353cf4da
Charles Blouin <cha.blouin@gmail.com>
2019-01-01 21:55:39 -0500

    Added communication script

--- RPi/script_3.sh
+++ RPi/script_3.sh
@@ -43,5 +43,26 @@
         echo sudo raspi-config
         echo Interface > VNC
         echo Install VNC viewer https://www.realvnc.com/en/connect/download/viewer/linux/ 
+        echo follow the password instruction section here: https://www.raspberrypi.org/documentation/remote-access/vnc/
+        # To install Arduino IDE
+        # cd Downloads/
+        #tar -xf arduino-1.8.3-linuxarm.tar.xz
+        # sudo mv arduino-1.8.8 /opt
+        # sudo /opt/arduino-1.8.8/install.sh
+        # TODO automate this part in script, see: https://arduino.stackexchange.com/questions/14771/arduino-command-line-interface-how-to-change-boardsmanager-additional-urls/14773
+        # Within VNC and the Adruino IDE, manually add a new board with https://files.pololu.com/arduino/package_pololu_index.json
+        # Set the new board and Port (USB should auto detect the board)
+        # Upload a test sketch such as Blink.ino and save it.
+        # Test the upload with command line in C9
+        # /opt/arduino-1.8.8/arduino --upload ~/Arduino/Blink/Blink.ino
+        # Add  the RPi slave library
+        # cd /home/pi/Arduino/libraries
+        # git clone https://github.com/pololu/pololu-rpi-slave-arduino-library
+        # mv pololu-rpi-slave-arduino-library PololuRPiSlave
+        # Add the A-star 32U4 library
+        # cd /home/pi/Arduino/libraries
+        # git clone https://github.com/pololu/balboa-32u4-arduino-library
+        # mv balboa-32u4-arduino-library Balboa32U4
+        
 	break
 	done
