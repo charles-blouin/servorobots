d0011728158176031c560caa4f1669f270f79851
Charles Blouin <cha.blouin@gmail.com>
2019-10-20 11:38:48 -0400

    step_test results were missing

--- balboa-arduino/characterization/step_test.py
+++ balboa-arduino/characterization/step_test.py
@@ -4,6 +4,7 @@ a_star = AStar()
 import time
 
 file_name = 'step_test.txt'
+gear_ratio = 1322
 fps = 100
 frame_length = 1/fps
 episode_length = 3 # in seconds
@@ -20,10 +21,10 @@ for step in range(0,fps*episode_length):
     timestamp.append(time.time() - start_time)
     
     current_encoder = a_star.read_encoders()
-    encoders.append(current_encoder)
+    encoders.append((current_encoder[0]/gear_ratio, current_encoder[1]/gear_ratio))
     
     velocities.append((current_encoder[0] - previous_encoder[0]/frame_length, current_encoder[1] - previous_encoder[1]/frame_length))
-    current_encoder = 
+    current_encoder = previous_encoder
     if step < 1*fps:
         a_star.motors(0,0)
         motors.append((0,0))
