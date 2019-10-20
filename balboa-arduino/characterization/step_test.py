11bcb23a2b163f09498138b644450da93938b9c6
Charles Blouin <cha.blouin@gmail.com>
2019-10-20 12:24:38 -0400

    improved the characterization test

--- balboa-arduino/characterization/step_test.py
+++ balboa-arduino/characterization/step_test.py
@@ -5,26 +5,34 @@ import time
 
 file_name = 'step_test.txt'
 gear_ratio = 1322
-fps = 100
+fps = 50
 frame_length = 1/fps
 episode_length = 3 # in seconds
 encoders = []
 motors = []
-timestamp = []
+timestamps = []
 velocities = []
+voltages = []
 
 start_time = time.time()
 
-previous_encoder = a_star.read_encoders()
+
+
+start_encoder = a_star.read_encoders()
+start_rot = [start_encoder[0]/ gear_ratio, start_encoder[1]/ gear_ratio]
+previous_rot = start_rot
 
 for step in range(0,fps*episode_length):
-    timestamp.append(time.time() - start_time)
+    timestamps.append(time.time() - start_time)
     
     current_encoder = a_star.read_encoders()
-    encoders.append((current_encoder[0]/gear_ratio, current_encoder[1]/gear_ratio))
+    current_rot = [current_encoder[0]/ gear_ratio - start_rot[0], current_encoder[1]/ gear_ratio - start_rot[1]]
+    encoders.append(current_rot)
     
-    velocities.append((current_encoder[0] - previous_encoder[0]/frame_length, current_encoder[1] - previous_encoder[1]/frame_length))
-    current_encoder = previous_encoder
+    velocities.append(((current_rot[0] - previous_rot[0])/frame_length, (current_rot[1] - previous_rot[1])/frame_length))
+    previous_rot = current_rot
+    
+    voltages.append(a_star.read_battery_millivolts()[0]/1000)
     if step < 1*fps:
         a_star.motors(0,0)
         motors.append((0,0))
@@ -44,7 +52,9 @@ for step in range(0,fps*episode_length):
     # a_star.motors(0, 0)
 
 with open(file_name, 'w') as filehandle:
-    json.dump({"timestamp": timestamp, "encoders": encoders, "motors": motors, "velocities": velocities}, filehandle)
+    json.dump({"timestamps": timestamps, "encoders": encoders, "motors": motors, "velocities": velocities, 
+            "voltages": voltages
+        }, filehandle)
     
 with open(file_name, 'r') as filehandle:
     test_result = json.load(filehandle)
