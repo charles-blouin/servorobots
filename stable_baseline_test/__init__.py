19f6ba15af704764c80f5f43a7e58582afea76b6
Charles Blouin <cha.blouin@gmail.com>
2019-11-07 00:50:50 -0500

    Merge branch 'master' of https://github.com/charles-blouin/servorobots
    # Conflicts:
    #	README.md

---
+++ stable_baseline_test/__init__.py
@@ -0,0 +1,20 @@
+from gym.envs.registration import register
+#from gym.scoreboard.registration import add_task, add_group
+'''
+register(
+    id='ServorobotsInvertedPendulum-v1',
+    entry_point='servorobots:gym_InvertedPendulum',
+    max_episode_steps=1000,
+    reward_threshold=950.0,
+    tags={ "pg_complexity": 1*1000000 },
+    )
+'''
+register(
+    id='InvertedPendulum-v0',
+    entry_point='stable-baseline-test:CartPoleEnv',
+    max_episode_steps=1000,
+    reward_threshold=950.0,
+    tags={ "pg_complexity": 1*1000000 },
+    )
+
+from stable-baseline-test.gym_modified_pendulum import CartPoleEnv
--- stable_baseline_test/play_balancer.py
+++ stable_baseline_test/play_balancer.py
@@ -2,7 +2,7 @@ import gym
 
 import time
 
-
+env = gym.make('InvertedPendulum-v0')
 
 from stable_baselines.common.policies import MlpPolicy
 from stable_baselines.common.vec_env import SubprocVecEnv
@@ -10,9 +10,10 @@ from stable_baselines import PPO2
 if __name__ == '__main__':
     # multiprocess environment
     n_cpu = 1
-    env = SubprocVecEnv([lambda: gym.make('CartPole-v1') for i in range(n_cpu)])
-    model = PPO2.load("ppo2_cartpole")
 
+    env = SubprocVecEnv([lambda: env for i in range(n_cpu)])
+    model = PPO2.load("ppo2_cartpole")
+    print(env)
     # Enjoy trained agent
     obs = env.reset()
     while True:
